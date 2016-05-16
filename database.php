be67d6f9a018bb01a65d2d7c9aacc407b8243d16
alice <leealice033@gmail.com>
2016-05-16 18:20:25 +0800

    update database.php:using Objective-oriented way to fetching data.

--- database.php
+++ database.php
@@ -1,59 +1,58 @@
 <?php
+/*物件導向-->撈取資料*/
 global $_DB;
 /*設定database連線資訊*/
 
 $_DB['host'] = "localhost";
 $_DB['username'] = "root";
 $_DB['password'] = 'mysql';
-$_DB['testDB'];
+$_DB['dbname']= "testDB";
 
 class Database
 {
 	var $_dbConn = 0;
 	var $_queryResource = 0;
 	/*建立資料庫連結 */
-	function connect_db($host, $user, $pwd, $dbname){
+	function connect_db($host, $user, $pwd, $dbname)
+	{
 		$dbConn = mysql_connect($host, $user, $pwd);
 		if(! $dbConn){
 			echo "db connect error!";
 		}
-		
-	}
-	/*
-	private static $dbName = 'testDB';
-	private static $dbHost = 'localhost';
-	private static $dbUsername = 'root';
-	private static $dbUsernamePassword = 'mysql';
+		else{
+			echo "db connect succesful! ";
+		}
+		/* 設定語系*/
+		//mysql_query("SET NAMES 'UTF8'");
+		/* 設定使用的資料庫 */
+    	if (!mysql_select_db($dbname, $dbConn)){
+       	echo "mysql_select_db error !!";
+       }
 
-	private static $cont = null;
-	public function _construct() {
-		die('Init funciont is not allowed');
-	}
+    	$this->_dbConn = $dbConn;
+    	return true;
+  	}
 
-	public static function connect()
-	{
-		// one connection through whole application
-		if( null == self::$cont)
-		{
-			try
-			{
-				self::$cont = new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);
+  	/* 建立SQL QUERY 操作*/
+  	function runQuery($sql){
+  		if(! $queryResource = mysql_query($sql,$this->_dbConn))
+  		{
+  			echo "mysql_query error!";
+  		}
+  		$this->_queryResource = $queryResource;
+  		return $queryResource;
+  	}
 
-			}
-			 catch(PDOException $e)
-        	{
-         	 die($e->getMessage()); 
-        	}
-       	}
-       return self::$cont;
-    }
+  	/*回傳query 資料使用 mysql_fetch_array*/
+  	function fetch_array(){
+  		return mysql_fetch_array($this->_queryResource,MYSQL_ASSOC);
+  	}
 
-     
-    public static function disconnect()
-    {
-        self::$cont = null;
-    }*/
+  	/*關閉資料庫連結 */
+  	function closeDB()
+  	{
+  		mysql_close($this->_dbConn);
+  	}
 
 }
 ?>
-
